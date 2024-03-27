f8f8c92a4d8232a4cdfb3f3f7898e9965d5b8a88
冴羽 <675261143@qq.com>
2024-03-27 15:43:54 +0800

    next-404

---
+++ app/(admin)/admin/page.js
@@ -0,0 +1,5 @@
+export default async function Page() {
+  return (
+    <div className="h-60 flex-1 rounded-xl bg-teal-400 text-white flex items-center justify-center m-6">Hello Admin!</div>
+  )
+}
---
+++ app/(mobile)/layout.js
@@ -0,0 +1,20 @@
+import "../globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head></head>
+      <body>
+      <div className="bg-indigo-600">
+            <div className="max-w-screen-xl mx-auto px-4 py-3 text-white sm:text-center md:px-8">
+                <p className="font-medium">
+                    We just launched a new version of our library! <a href="#" className="font-semibold underline duration-150 hover:text-indigo-100 inline-flex items-center gap-x-1">
+                        Learn more
+                    </a>
+                </p>
+            </div>
+        </div>
+        {children}</body>
+    </html>
+  );
+}
