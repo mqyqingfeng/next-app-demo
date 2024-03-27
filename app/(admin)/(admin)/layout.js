eb27b8c8d2739de92c6bad0cab6ca0737325655d
冴羽 <675261143@qq.com>
2024-03-27 18:00:55 +0800

    next-404-4

--- app/(admin)/(admin)/layout.js
+++ app/(admin)/(admin)/layout.js
@@ -1,4 +1,4 @@
-import "../globals.css";
+import "../../globals.css";
 
 const navigation = [{ title: "Features", path: "#" }, { title: "Integrations", path: "#" }, { title: "Customers", path: "#" }, { title: "Pricing", path: "#" }];
 
