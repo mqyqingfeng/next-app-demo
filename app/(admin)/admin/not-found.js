cc03206590b2deda62054a42f995fc66898f589f
冴羽 <675261143@qq.com>
2024-03-27 17:31:02 +0800

    next-404-3

--- app/(admin)/admin/not-found.js
+++ app/(admin)/admin/not-found.js
@@ -4,7 +4,7 @@ export default function NotFound() {
       <div className="mx-auto px-4 flex items-center justify-start px-8 mt-6">
         <div className="max-w-lg mx-auto text-center">
           <h3 className="text-gray-800 text-4xl font-semibold sm:text-5xl">
-            Page not found
+            Admin Page not found
           </h3>
           <p className="text-gray-600 mt-3">
             Sorry, the page you are looking for could not be found or has been
